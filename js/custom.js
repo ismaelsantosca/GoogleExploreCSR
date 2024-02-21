32feb81a453db691deeb366164de74e0e20b69e3
Ismael Santos <99123@globalia.com>
2024-02-21 18:09:41 +0100

    added dynamic nav active

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,20 @@
 $( document ).ready(function() {
    
-    fetch("./nav/nav.html")
+    fetch('./nav/nav.html')
   .then(response => {
     return response.text()
   })
-  .then(data => {
+  .then( data => {
+    let path = window.location.pathname.replace('/','').split('/');
+    let classActive = 'home'; 
+    if(path.length > 1 && path[1] != ''){
+        classActive =  path[1];
+    }
+
+    $(data).find('#navbarSupportedContent .active').removeClass('active');
+    $(data).find('.nav-item[data-item="about"]').addClass('active');
+
+  }).then(data => {
     document.querySelector("#navbarSupportedContent").innerHTML = data;
   });
 });
